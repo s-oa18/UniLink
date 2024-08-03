c4421886e4fef62a26f6c9bc5f9d7eddb32d91b5
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2024-08-03 03:05:08 +0100

    Azure db connection and dashboard updates

--- php/config.php
+++ php/config.php
@@ -1,11 +1,30 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "root";
+  // Database connection details
+  $hostname = "unilink-server.mysql.database.azure.com";
+  $username = "sellontech";
+  $password = "@Checkers124";
   $dbname = "unilinkdb";
+  
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
+  // Path to the SSL CA certificate
+  $ssl_ca = "/Applications/DigiCertGlobalRootCA.crt.pem";
+
+  // Initialize the MySQL connection
+  $conn = mysqli_init();
+
+  // Set SSL options
+  mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
+
+  // Establish the connection
+  if (!mysqli_real_connect($conn, $hostname, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL)) {
+    die("Database connection error: " . mysqli_connect_error());
   }
+
+  echo "Successfully connected to Azure Database for MySQL.";
+
+  // Close the connection
+  mysqli_close($conn);
 ?>
+
+
+
